9250080ae0ee3a6c5b99a6dab9ac7c14671c8217
Alessio Amstutz <alessio.amstutz@students.bfh.ch>
2025-05-07 15:12:19 +0200

    js dom elemetn

--- FortuneRoulette/js/script.js
+++ FortuneRoulette/js/script.js
@@ -12,4 +12,19 @@ async function loadTimetable() {
 const timetable = await loadTimetable();
 console.log(timetable); // gibt die Daten der API oder false in der Konsole aus
 
-const advice = 
+// 2. DOM ELEMENTE LADEN
+const adviceText = document.querySelector('#randomAdvice');
+
+const Text 
+
+
+const cards_container = document.querySelector('#cards');
+all_pokemon_with_details.forEach(pokemon => {
+    const card = `<div class="card">
+                <h2 class="pokemon_name">${pokemon.name}</h2>
+                <img class="pokemon_image" src="${pokemon.image}" alt="">
+                <p class="pokemon_hp">Lebenskraft: <span class="value_hp">${pokemon.hp}</span></p>
+            </div>`
+    cards_container.innerHTML += card;
+})
+
