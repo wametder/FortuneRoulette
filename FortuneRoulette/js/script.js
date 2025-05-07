1d802c0ccc9c36408a95ad25c7331523edde9845
Nicolas Egger <nicolas.egger@stud.fhgr.ch>
2025-05-07 14:27:17 +0200

    FortuneRoulette

---
+++ FortuneRoulette/js/script.js
@@ -0,0 +1,13 @@
+// 1. API LADEN
+async function loadTimetable() {
+    const url = 'https://api.adviceslip.com/advice/search/try';
+    try {
+        const response = await fetch(url);
+        return await response.json();
+    } catch (error) {
+        console.error(error);
+        return false;
+    }
+}
+const timetable = await loadTimetable();
+console.log(timetable); // gibt die Daten der API oder false in der Konsole aus
