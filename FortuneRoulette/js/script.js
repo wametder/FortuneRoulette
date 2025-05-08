013bd5f211d165fa34d2ed067144d81aba6b991b
Alessio Amstutz <alessio.amstutz@students.bfh.ch>
2025-05-08 09:40:38 +0200

    random gewinn und verloren etc anpassung

--- FortuneRoulette/js/script.js
+++ FortuneRoulette/js/script.js
@@ -42,3 +42,16 @@ const beleidigung = `<div class="randomInsult">
             <h2 class="insult">${timetable2.insult}</h2>
         </div>`;
 insultText.innerHTML += beleidigung;
+
+
+
+    function spiele(wahl) {
+        const ergebnisse = ['gewonnen', 'unentschieden', 'verloren'];
+        const zufall = Math.floor(Math.random() * ergebnisse.length);
+        const ergebnis = ergebnisse[zufall];
+            
+        // Seitenname z. B. schere_gewonnen.html
+        const zielseite = `${wahl}_${ergebnis}.html`;
+
+        window.location.href = zielseite;
+     }
