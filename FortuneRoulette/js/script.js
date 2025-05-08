078e8e387d0f661bd45b91afd4d040d6a122946b
Nicolas Egger <nicolas.egger@stud.fhgr.ch>
2025-05-08 10:57:56 +0200

    igwas

--- FortuneRoulette/js/script.js
+++ FortuneRoulette/js/script.js
@@ -45,16 +45,14 @@ insultText.innerHTML += beleidigung;
 
 
 
-    function spiele(wahl) {
-        const ergebnisse = ['gewonnen', 'unentschieden', 'verloren'];
-        const zufall = Math.floor(Math.random() * ergebnisse.length);
-        const ergebnis = ergebnisse[zufall];
-            
-        // Seitenname z. B. schere_gewonnen.html
-        const zielseite = `${wahl}/${wahl}_${ergebnis}.html`;
+function spiele(wahl) {
+    const ergebnisse = ['gewonnen', 'unentschieden', 'verloren'];
+    const zufall = Math.floor(Math.random() * ergebnisse.length);
+    const ergebnis = ergebnisse[zufall];
+        
+    // Seitenname z. B. schere_gewonnen.html
+    const zielseite = `${wahl}/${wahl}_${ergebnis}.html`;
 
-        window.location.href = zielseite;
-     }
-
-
+    window.location.href = zielseite;
+}
      window.spiele = spiele;
